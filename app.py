9721be3c4130f27b18ac51a0d2e2abce9fe11772
vickyqian <vickyqry@gmail.com>
2017-10-04 11:55:57 -0400

    Update app.py
    added postgresql

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect
-
+from flask.ext.sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/VickyQian'
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
@@ -8,3 +10,4 @@ def index():
 
 if __name__ == '__main__':
   app.run(port=33509)
+  
